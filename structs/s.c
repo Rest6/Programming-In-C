b07a99eab6de4d6f49932085dfa9bc67482d6dce
Igor Broychenko <ibroychenko@gmail.com>
2018-11-06 14:33:40 +0200

    added structs

--- structs/s.c
+++ structs/s.c
@@ -4,7 +4,7 @@
 
 #define CLIENTS 3
 
-int main(void)
+int main(int argc, char* argv[])
 {
 
 client clients[CLIENTS];
@@ -12,9 +12,28 @@ client clients[CLIENTS];
 for(int i = 0; i < CLIENTS; i++)
 {
 printf("Client's name: \n");
-clients[i].name = "Test";
+clients[i].name = argv[1];
+
+printf("Client's address: \n");
+clients[i].address = argv[2];
 
 printf("Client's name: %s\n", clients[i].name);
+}
+
+FILE* file = fopen("clients.csv", "w");
+if(file != NULL)
+{
+	for(int i = 0; i < CLIENTS; i++)
+	{
+	fprintf(file, "%s,%s\n", clients[i].name, clients[i].address);
+	}
+	fclose(file);
+}
+
+for(int i = 0; i < CLIENTS; i++)
+{
+	free(clients[i].name);
+	free(clients[i].address);
 }
 
 return 0;
