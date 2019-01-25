15fd46b13a571a60e2285f865c5acb4c3cce238b
Igor Broychenko <ibroychenko@gmail.com>
2019-01-25 10:35:36 +0200

    added upper function

--- main/basic/task1/read.c
+++ main/basic/task1/read.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+char* last(char*);
+char* upper(char*);
 
 int main()
 {
@@ -15,5 +17,48 @@ int main()
 		name[10] = name[strlen(name)-1];
 	}
 	printf("Username: %s\n",name);
+	printf("Result of lastName function: %s\n",last(name));
+	printf("Uppercase word: %s\n", upper(name));
 	return 0;
 }
+
+char* last(char* lastName)
+{
+	char test[10] = "";
+	int i = 0;
+
+	printf("Brfote all %s \n",test);
+	
+	for(i = 0; i < strlen(lastName); i++)
+	{
+		if(lastName[i] == ' ')
+		{
+			while(scanf("%c",lastName) != '!')
+			{
+				test[i] = lastName[i];
+			
+			}
+		}
+	}
+
+	printf("func result: %s\n",test);	
+	lastName = test;
+	return lastName;
+}
+
+char* upper(char str[])
+{
+	int c = 0;
+
+	while (str[c] != '\0')
+	{
+	if(str[c] >= 'a' && str[c] <= 'z')
+	{
+		str[c] = str[c] - 32;
+	}
+		c++;
+	}
+	return str;
+}
+
+
